241bd288d76bdf28b540dae5bda2274da2f97451
Alex Franco <aadf82@gmail.com>
2018-10-11 13:26:22 -0400

    Added custom markup.

--- wpsl-templates/wpsl-custom.php
+++ wpsl-templates/wpsl-custom.php
@@ -1,14 +1,14 @@
 <?php
 global $wpsl_settings, $wpsl;
 
-$output         = $this->get_custom_css();
+// $output         = $this->get_custom_css();
 $autoload_class = ( !$wpsl_settings['autoload'] ) ? 'class="wpsl-not-loaded"' : '';
 
-$output .= '<div id="wpsl-wrap">' . "\r\n";
-$output .= "\t" . '<div class="wpsl-search wpsl-clearfix ' . $this->get_css_classes() . '">' . "\r\n";
+// $output .= '<div id="wpsl-wrap">' . "\r\n";
+$output .= "\t" . '<div class="inner-row">' . "\r\n";
 $output .= "\t\t" . '<div id="wpsl-search-wrap">' . "\r\n";
 $output .= "\t\t\t" . '<form autocomplete="off">' . "\r\n";
-$output .= "\t\t\t" . '<div class="wpsl-input">' . "\r\n";
+$output .= "\t\t\t" . '<div class="wpsl-input cell small-6">' . "\r\n";
 $output .= "\t\t\t\t" . '<div><label for="wpsl-search-input">' . esc_html( $wpsl->i18n->get_translation( 'search_label', __( 'Your location', 'wpsl' ) ) ) . '</label></div>' . "\r\n";
 $output .= "\t\t\t\t" . '<input id="wpsl-search-input" type="text" value="' . apply_filters( 'wpsl_search_input', '' ) . '" name="wpsl-search-input" placeholder="" aria-required="true" />' . "\r\n";
 $output .= "\t\t\t" . '</div>' . "\r\n";
@@ -17,7 +17,7 @@ if ( $wpsl_settings['radius_dropdown'] || $wpsl_settings['results_dropdown']  ) 
     $output .= "\t\t\t" . '<div class="wpsl-select-wrap">' . "\r\n";
 
     if ( $wpsl_settings['radius_dropdown'] ) {
-        $output .= "\t\t\t\t" . '<div id="wpsl-radius">' . "\r\n";
+        $output .= "\t\t\t\t" . '<div id="wpsl-radius cell auto">' . "\r\n";
         $output .= "\t\t\t\t\t" . '<label for="wpsl-radius-dropdown">' . esc_html( $wpsl->i18n->get_translation( 'radius_label', __( 'Search radius', 'wpsl' ) ) ) . '</label>' . "\r\n";
         $output .= "\t\t\t\t\t" . '<select id="wpsl-radius-dropdown" class="wpsl-dropdown" name="wpsl-radius">' . "\r\n";
         $output .= "\t\t\t\t\t\t" . $this->get_dropdown_list( 'search_radius' ) . "\r\n";
@@ -26,7 +26,7 @@ if ( $wpsl_settings['radius_dropdown'] || $wpsl_settings['results_dropdown']  ) 
     }
 
     if ( $wpsl_settings['results_dropdown'] ) {
-        $output .= "\t\t\t\t" . '<div id="wpsl-results">' . "\r\n";
+        $output .= "\t\t\t\t" . '<div id="wpsl-results cell auto">' . "\r\n";
         $output .= "\t\t\t\t\t" . '<label for="wpsl-results-dropdown">' . esc_html( $wpsl->i18n->get_translation( 'results_label', __( 'Results', 'wpsl' ) ) ) . '</label>' . "\r\n";
         $output .= "\t\t\t\t\t" . '<select id="wpsl-results-dropdown" class="wpsl-dropdown" name="wpsl-results">' . "\r\n";
         $output .= "\t\t\t\t\t\t" . $this->get_dropdown_list( 'max_results' ) . "\r\n";
@@ -41,13 +41,12 @@ if ( $this->use_category_filter() ) {
     $output .= $this->create_category_filter();
 }
 
-$output .= "\t\t\t\t" . '<div class="wpsl-search-btn-wrap"><input id="wpsl-search-btn" type="submit" value="' . esc_attr( $wpsl->i18n->get_translation( 'search_btn_label', __( 'Search', 'wpsl' ) ) ) . '"></div>' . "\r\n";
+$output .= "\t\t\t\t" . '<div class="wpsl-search-btn-wrap"><input id="wpsl-search-btn" class="button" type="submit" value="' . esc_attr( $wpsl->i18n->get_translation( 'search_btn_label', __( 'Search', 'wpsl' ) ) ) . '"></div>' . "\r\n";
 
-$output .= "\t\t" . '</form>' . "\r\n";
-$output .= "\t\t" . '</div>' . "\r\n";
-$output .= "\t" . '</div>' . "\r\n";
+$output .= "\t\t" . '</form>' . "\r\n"; // End form
+$output .= "\t\t" . '</div>' . "\r\n"; // End #wpsl-search-wrap
+$output .= "\t" . '</div>' . "\r\n"; // End inner-row
 
-$output .= "\t" . '<div id="wpsl-gmap" class="wpsl-gmap-canvas"></div>' . "\r\n";
 
 $output .= "\t" . '<div id="wpsl-result-list">' . "\r\n";
 $output .= "\t\t" . '<div id="wpsl-stores" '. $autoload_class .'>' . "\r\n";
@@ -58,10 +57,12 @@ $output .= "\t\t\t" . '<ul></ul>' . "\r\n";
 $output .= "\t\t" . '</div>' . "\r\n";
 $output .= "\t" . '</div>' . "\r\n";
 
+$output .= "\t" . '<div id="wpsl-gmap" class="wpsl-gmap-canvas responsive-embed"></div>' . "\r\n";
+
 if ( $wpsl_settings['show_credits'] ) {
     $output .= "\t" . '<div class="wpsl-provided-by">'. sprintf( __( "Search provided by %sWP Store Locator%s", "wpsl" ), "<a target='_blank' href='https://wpstorelocator.co'>", "</a>" ) .'</div>' . "\r\n";
 }
 
-$output .= '</div>' . "\r\n";
+// $output .= '</div>' . "\r\n"; // end wpsl-wrap
 
 return $output;
