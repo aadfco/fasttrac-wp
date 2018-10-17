c113423fc3982c90b1962780258c8f311d002640
Alex Franco <aadf82@gmail.com>
2018-10-17 10:48:57 -0400

    Removed unused markup.

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -3,16 +3,16 @@
  * Template part for displaying page content in page.php
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
+ * 	<header class="entry-header">
+ *	<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+ *	</header><!-- .entry-header -->
  * @package Fast_Trac
  */
 
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+
 
 	<?php fasttrac_post_thumbnail(); ?>
 
@@ -27,26 +27,4 @@
 		?>
 	</div><!-- .entry-content -->
 
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'fasttrac' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					get_the_title()
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
 </article><!-- #post-<?php the_ID(); ?> -->
