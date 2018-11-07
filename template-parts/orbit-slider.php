c9ceae60eda204d477dbb1883f0c7f6543783799
Alex Franco <aadf82@gmail.com>
2018-11-07 13:58:57 -0500

    Changed markup position for orbit-wrapper. Added if statements for vars.

--- template-parts/orbit-slider.php
+++ template-parts/orbit-slider.php
@@ -1,8 +1,9 @@
 <!-- Begin Image Slider -->
 <div class="fullscreen-image-slider animated fadeIn">
   <div class="orbit" role="region" aria-label="FullScreen Pictures" data-orbit data-options="data-timer-delay: 7000; animInFromLeft:fade-in; animInFromRight:fade-in; animOutToLeft:fade-out; animOutToRight:fade-out;">
+    <div class="orbit-wrapper">
     <?php if( have_rows('orbit_slider') ): ?>
-    <ul class="orbit-container orbit-wrapper">
+    <ul class="orbit-container">
       <button class="orbit-previous">
         <span class="show-for-sr">Previous Slide</span>
         <span class="nav fa fa-chevron-left fa-3x"></span>
@@ -25,11 +26,22 @@
       <?php if( $slide_img ): ?>
         <li class="<?php echo $i == 0 ? 'is-active' : '' ?> orbit-slide">
           <figure class="orbit-figure">
-              <?php echo wp_get_attachment_image($slide_img, 'full', false, array( 'class' => 'orbit-image' ) ); ?>
+            <?php echo wp_get_attachment_image($slide_img, 'full', false, array( 'class' => 'orbit-image' ) ); ?>
+
             <figcaption class="orbit-caption">
+
+              <?php if( $slide_heading ): ?>
               <h1><?php echo $slide_heading; ?></h1>
+              <?php endif; ?>
+
+              <?php if( $slide_subheading ): ?>
               <p><?php echo $slide_subheading; ?></p>
+              <?php endif; ?>
+
+              <?php if( $slide_button_link ): ?>
               <a href="<?php echo $slide_button_link; ?>" class="button"><?php echo $slide_button_text; ?></a>
+              <?php endif; ?>
+
             </figcaption>
           </figure>
       <?php endif; ?>
@@ -42,6 +54,7 @@
 
     </ul>
     <?php endif; ?>
+    </div>
 
   <nav class="orbit-bullets">
     <?php $slide_count = count(get_field('orbit_slider')); ?>
