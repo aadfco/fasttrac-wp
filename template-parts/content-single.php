3fff5e243c4081d9b0aefa6a37d49805a65333a1
Alex Franco <aadf82@gmail.com>
2018-12-04 11:03:52 -0500

    Indenting and spacing

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -9,12 +9,19 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
 	<div class="post-content-wrapper">
+
 		<div class="container">
+
 			<div class="inner-row align-center">
+
 				<div class="post-content">
+
 					<?php
+
 					the_content( sprintf(
+
 						wp_kses(
 							/* translators: %s: Name of current post. Only visible to screen readers */
 							__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'fasttrac' ),
@@ -24,15 +31,23 @@
 								),
 							)
 						),
+
 						get_the_title()
+
 					) );
+
 					wp_link_pages( array(
 						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'fasttrac' ),
 						'after'  => '</div>',
 					) );
+
 					?>
 				</div>
+
 			</div>
+
 		</div>
+
 	</div><!-- .entry-content -->
+
 </article><!-- #post-<?php the_ID(); ?> -->
