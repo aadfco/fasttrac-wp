9b000a7d472a8322889c41e09ecd0866a80eb21c
Alex Franco <aadf82@gmail.com>
2019-07-03 11:48:08 -0400

    ACF enabled, txt-domain.

--- page-about-us.php
+++ page-about-us.php
@@ -1,11 +1,11 @@
 <?php get_header(); ?>
 <?php
-$featuredImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'xxl' );
+$featuredImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'xlarge' );
  ?>
 
 <div class="page-hero" style="background-color: #e31b23; background-image: url('<?php echo $featuredImg[0]; ?>');">
   <div class="page-hero-content centered">
-    <h1 class="standard-shadow">about us</h1>
+    <h1 class="standard-shadow"><?php the_title( __( '', 'fast-trac' ) ); ?></h1>
     <!-- <p class="subheader">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p> -->
   </div>
 </div>
@@ -13,52 +13,71 @@ $featuredImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'x
 <section class="filter-tabs inner-row align-center">
   <div class="container">
     <div class="inner-row align-center">
-      <ul class="tabs" data-tabs id="example-tabs">
-        <li class="tabs-title is-active"><a href="#who" aria-selected="true">who we are</a></li>
-        <li class="tabs-title"><a data-tabs-target="what" href="#what">what we do</a></li>
-        <li class="tabs-title"><a data-tabs-target="mission" href="#mission">our mission</a></li>
+
+    <?php if( have_rows('about_tabs') ): 
+        $row_name = get_row();
+        $sanitize_row = sanitize_title( $row_name );
+    ?>
+      <ul class="tabs" data-tabs id="about-tabs">
+        <?php $i=0; while( have_rows('about_tabs') ): the_row(); 
+            
+            $link = get_sub_field('tab_title_link');
+            $content = get_sub_field('tab_content');
+            $sanitize = sanitize_title( $link );
+        ?>
+        <li class="tabs-title <?php if($i==0) echo 'is-active'; ?>"><a data-tabs-target="<?php echo $sanitize; ?>" href="#<?php echo $sanitize; ?>" href="#<?php echo $sanitize; ?>" aria-selected="true"><?php echo $link; ?></a></li>
+        <?php $i++; endwhile; ?>
       </ul>
 
-      <div class="tabs-content" data-tabs-content="example-tabs">
-        <div class="tabs-panel is-active animated fadeIn" id="who">
-          <p>Fast Trac is about providing a revved up convenience experience to its customers while being active in the communities we work in. Fast Trac is a sure stop for all of your daily needs while on the go. No matter how fast paced your day is, Fast Trac has quality fuels for your cars, as well as everything you need to refuel your body to take on whatever comes your way. No matter who you are, or what you do, you have a place at Fast Trac. There just isnt enough time for you to be running across town to get what you need, and we know that, so we have created a number of locations for you to conveniently be able to stop and get what you need, when you need it.</p>
+      <div class="tabs-content" data-tabs-content="about-tabs">
+
+      <?php  $i=0; while( have_rows('about_tabs') ): the_row(); 
+       
+        $link = get_sub_field('tab_title_link');
+        $content = get_sub_field('tab_content');
+        $sanitize = sanitize_title( $link );
+      ?>
+        <div class="tabs-panel animated fadeIn <?php if($i==0) echo 'is-active'; ?>" id="<?php echo $sanitize; ?>">
+          <p><?php echo $content; ?></p>
         </div>
-        <div class="tabs-panel animated fadeIn" id="what">
-          <p>Fast Trac does what you need. We provide our customers with a warm and friendly atmosphere, where they can get the things they need, when they need it fast. With multiple locations through out the Youngstown, Ohio area.</p>
-        </div>
-        <div class="tabs-panel animated fadeIn" id="mission">
-          <p>Our mission is to provide our customers with the quality products they want and need, in a clean and friendly environment, with great customer service.</p>
-        </div>
+        <?php $i++; endwhile; endif; ?>
+
       </div>
     </div>
   </div>
 </section>
 
 <section class="stacks">
+<?php 
+    $storyIMG = get_field('story_image');
+    $storyIMGArray = wp_get_attachment_image_src($storyIMG, 'large');
+    $storyTitle = get_field('story_title');
+    $storyContent = get_field('story_content');
+?>
   <div class="stacked-wrapper inner-row border-top" data-equalizer>
-    <div class="stack-media large-order-2 wow animated fadeIn" role="img" aria-label="Success Image" data-equalizer-watch style="background-color: #e31b23; background-image: url('<?php echo $featuredImg[0]; ?>');">
-      <!-- <img src="img/fasttrac-day@1000px.jpg" alt=""> -->
+    <div class="stack-media large-order-2 wow animated fadeIn" role="img" aria-label="Success Image" data-equalizer-watch style="background-color: #e31b23; background-image: url('<?php echo $storyIMGArray[0]; ?>');">
     </div>
     <div class="stack-content container large-order-1 wow animated fadeIn" data-equalizer-watch>
-      <h1>our success story</h1>
-      <p>Fast Trac is a convenience store chain based in Youngstown, Ohio. For nearly twenty years, our mission at Fast Trac has been to meet the growing needs of the customers. Obviously, things have continued to change over the years. Life has become faster and much busier, and our customers expect us to be there to help them along the way. One thing that hasn't changed is our commitment to our customers, employees and especially the communities in which we operate on a daily basis.</p>
-      <p>Hadi A. Hadi and Dan Qutail founded the Fast Trac convenience store chain after coming together with an idea to create a local convenience store chain in the the town they both had grown up in. The first location that was purchased was located in Liberty Township where Hadi and Dan both worked together to operate. This location was operated under Xpress Fuel Mart.</p>
-      <p>It was not until October of 2006 that the second store was opened. By 2009 they were operating six locations in the Youngstown, Ohio market. under the name Xpress Convenience Stores. In 2010 Fast Trac was born and they purchased 15 more locations in the Youngstown, Ohio market. Literally tripling the size of the company.</p>
-      <p>In 2016 the company built its first ground up Fast Trac Convenience Store located in Austintown, Ohio which also has a Subway and a local coffee shop chain.</p>
-      <p>Continuing the successful growth, the second ground up location was built in 2017. Hadi and Dan continue to handle the day to day operations, both managing different aspects of the company while building a strong local team and being active in the communities they operate in.</p>
+      <h1><?php echo $storyTitle; ?></h1>
+      <p><?php echo $storyContent; ?></p>
     </div>
 
   </div> <!-- end stacked-wrapper -->
 </section>
 
 <div class="about-counter-bar">
+<?php
+    $customers = get_field('customer_counter');
+    $locations = get_field('locations_counter');
+    $awards = get_field('awards_counter');
+?>
   <div class="container">
     <div class="inner-row counter-items-container wow animatied flipInX">
       <div class="counter-item">
         <span class="counter-icon">
           <i class="fas fa-smile"></i>
         </span>
-        <span class="counter-number" data-count="1000000">
+        <span class="counter-number" data-count="<?php echo $customers; ?>">
           0
         </span>
         <span class="counter-text">
@@ -70,7 +89,7 @@ $featuredImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'x
         <span class="counter-icon">
           <i class="fas fa-building"></i>
         </span>
-        <span class="counter-number" data-count="31">
+        <span class="counter-number" data-count="<?php echo $locations; ?>">
           0
         </span>
         <span class="counter-text">
@@ -82,7 +101,7 @@ $featuredImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'x
         <span class="counter-icon">
           <i class="fas fa-trophy"></i>
         </span>
-        <span class="counter-number" data-count="110">
+        <span class="counter-number" data-count="<?php echo $awards; ?>">
           0
         </span>
         <span class="counter-text">
