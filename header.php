367c6cedba94a8585b99434e1977916c09fcdbff
Alex Franco <aadf82@gmail.com>
2019-11-27 12:46:01 -0500

    Tab size fix

--- header.php
+++ header.php
@@ -22,60 +22,60 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 	<header class="navigation">
-      <div class="top-nav">
-        <div class="full-container">
-          <div class="inner-row align-justify">
-            <div class="top-social">
-              <ul class="menu">
-								<?php if( have_rows('social_media_icons','option') ): ?>
-									<?php while( have_rows('social_media_icons','option') ): the_row();
-										// ACF Repeater Variables
-										$social_media_link = get_sub_field('social_media_link', 'option');
-										$social_media_icon = get_sub_field('social_media_icon', 'option');
-										?>
-											<li>
-												<a href="<?php echo $social_media_link; ?>" class="<?php echo $social_media_icon; ?>"></a>
-											</li>
-									<?php endwhile; ?>
-								<?php endif; ?>
-              </ul>
-            </div>
-            <nav class="secondary-menu" role="navigation">
-							<?php
-								wp_nav_menu( array(
-									'theme_location' => 'secondary-menu',
-									'menu_id'        => 'secondary-menu',
-									'menu_class'		 => 'menu',
-									'container'			 => 'ul'
-								) );
-							?>
-            </nav>
-          </div>
-        </div>
-      </div><!-- end top-nav -->
+		<div class="top-nav">
+			<div class="full-container">
+				<div class="inner-row align-justify">
+					<div class="top-social">
+						<ul class="menu">
+							<?php if( have_rows('social_media_icons','option') ): ?>
+								<?php while( have_rows('social_media_icons','option') ): the_row();
+									// ACF Repeater Variables
+									$social_media_link = get_sub_field('social_media_link', 'option');
+									$social_media_icon = get_sub_field('social_media_icon', 'option');
+									?>
+										<li>
+											<a href="<?php echo $social_media_link; ?>" class="<?php echo $social_media_icon; ?>"></a>
+										</li>
+								<?php endwhile; ?>
+							<?php endif; ?>
+						</ul>
+					</div>
+					<nav class="secondary-menu" role="navigation">
+						<?php
+							wp_nav_menu( array(
+								'theme_location' => 'secondary-menu',
+								'menu_id'        => 'secondary-menu',
+								'menu_class'		 => 'menu',
+								'container'			 => 'ul'
+							) );
+						?>
+					</nav>
+				</div>
+			</div>
+		</div><!-- end top-nav -->
 
       <!-- Begin Main Nav -->
-      <div class="main-nav">
-        <div class="full-container">
-          <div class="inner-row align-justify align-middle">
-            <div class="logo animated">
+		<div class="main-nav">
+				<div class="full-container">
+					<div class="inner-row align-justify align-middle">
+						<div class="logo animated">
 
 							<?php
 							the_custom_logo();
 							if ( is_front_page() && is_home() ) :
 							?>
 							<?php endif; ?>
-            </div>
+						</div>
 
-            <nav class="mobile-menu hide-for-large" role="navigation">
-              <div class="mobile-btn-wrap animated">
-                <button class="hamburger hamburger--elastic" type="button" data-toggle="mobile-menu-overlay">
-                  <span class="hamburger-box">
-                    <span class="hamburger-inner"></span>
-                  </span>
-                </button>
-              </div>
-              <div class="mobile-menu-overlay transition-duration-300ms" role="navigation" id="mobile-menu-overlay" data-toggler data-animate="hinge-in-from-top hinge-out-from-top">
+						<nav class="mobile-menu hide-for-large" role="navigation">
+							<div class="mobile-btn-wrap animated">
+								<button class="hamburger hamburger--elastic" type="button" data-toggle="mobile-menu-overlay">
+								<span class="hamburger-box">
+									<span class="hamburger-inner"></span>
+								</span>
+								</button>
+							</div>
+							<div class="mobile-menu-overlay transition-duration-300ms" role="navigation" id="mobile-menu-overlay" data-toggler data-animate="hinge-in-from-top hinge-out-from-top">
 								<?php
 									wp_nav_menu( array(
 										'theme_location' => 'primary-menu',
@@ -92,11 +92,11 @@
 										'container'			 => 'ul'
 									) );
 								?>
-              </div>
-            </nav>
+							</div>
+						</nav>
 
-            <!-- Begin Desktop Navigation Menus -->
-            <nav class="primary-menu animated">
+						<!-- Begin Desktop Navigation Menus -->
+						<nav class="primary-menu animated">
 							<?php
 								wp_nav_menu( array(
 									'theme_location' => 'primary-menu',
@@ -105,26 +105,26 @@
 									'container'			 => 'ul'
 								) );
 							?>
-            </nav>
+						</nav>
 
-            <div class="fp-login animated">
+						<div class="fp-login animated">
 							<?php
 							// ACF Fast Points Logo Variables
 							$fp_logo = get_field('fast_points_logo', 'option');
 							$fp_logo_link = get_field('fast_points_link', 'option')
 							?>
-              <div class="fp-logo">
-                <a href="<?php echo $fp_logo_link['url']; ?>" target="<?php echo $fp_logo_link['target']; ?>">
+							<div class="fp-logo">
+								<a href="<?php echo $fp_logo_link['url']; ?>" target="<?php echo $fp_logo_link['target']; ?>">
 									<?php
 										if( !empty($fp_logo) ): ?>
 											<img src="<?php echo $fp_logo['url']; ?>" alt="<?php echo $fp_logo['alt']; ?>" />
 									<?php endif; ?>
-                  <span class="fp-login-link">Login | Sign Up</span>
-                </a>
-              </div>
-            </div>
+								<span class="fp-login-link">Login | Sign Up</span>
+								</a>
+							</div>
+						</div>
 
-          </div> <!-- end inner-row -->
-        </div> <!-- end container -->
-      </div> <!-- end main-nav -->
+					</div> <!-- end inner-row -->
+				</div> <!-- end full-container -->
+		</div> <!-- end main-nav -->
     </header> <!-- end navigation -->
